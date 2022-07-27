cb35c658658b89dc883225f6521b20397b7b3208
Jude Adesulu <judeadesuluflyer6@gmail.com>
2022-07-27 07:01:58 +0100

    Set up aplication

---
+++ src/common/constants/config.constant.ts
@@ -0,0 +1,9 @@
+export const configConstant = {
+    mail: {
+      host: 'MAIL_HOST',
+      port: 'MAIL_PORT',
+      username: 'MAIL_USERNAME',
+      password: 'MAIL_PASSWORD',
+    },
+  };
+  
